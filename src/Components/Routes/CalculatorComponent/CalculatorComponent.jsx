2234ceb7ffe426016afec08e304a1a0494c054f3
Prabanjan Jeyasankar <prabanjanjeyasankar@gmail.com>
2023-04-10 15:22:40 +0530

    Modify. modified the calculator alignment.

--- src/Components/Routes/CalculatorComponent/CalculatorComponent.jsx
+++ src/Components/Routes/CalculatorComponent/CalculatorComponent.jsx
@@ -20,11 +20,10 @@ function CalculatorComponent() {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="calculator flex flex-col justify-center items-center h-screen">
 
-      <div className="buttons grid grid-cols-4 gap-2 p-4">
-      <div className="display bg-gray-900 text-white text-right p-4 text-4xl font-bold">{input || '0'}</div>
-        <button value="C" className="bg-gray-600 text-white font-bold text-2xl p-2 rounded-full col-span-2" onClick={handleClearButtonClick}>C</button> <br />
+      <div className="buttons grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-2 p-4">
+      <div className="display bg-gray-900 text-white text-right p-4 text-4xl font-bold col-span-4">{input || '0'}</div>
         <button value="/" className="bg-orange-400 text-white font-bold text-2xl p-2 rounded-full" onClick={handleButtonClick}>÷</button>
         <button value="7" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>7</button>
         <button value="8" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>8</button>
@@ -39,8 +38,10 @@ function CalculatorComponent() {
         <button value="3" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>3</button>
         <button value="+" className="bg-orange-400 text-white font-bold text-2xl p-2 rounded-full" onClick={handleButtonClick}>+</button>
         <button value="0" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg col-span-2" onClick={handleButtonClick}>0</button>
-        <button value="." className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>.</button>
+        <button value="." className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleButtonClick}>.</button><br />
+        <button value="C" className="bg-gray-600 text-white font-bold text-2xl p-2 rounded-full col-span-2" onClick={handleClearButtonClick}>C</button>
         <button value="=" className="bg-gray-300 text-black font-bold text-2xl p-2 rounded-lg" onClick={handleCalculateButtonClick}>=</button>
+
 </div>
 
       </div>
